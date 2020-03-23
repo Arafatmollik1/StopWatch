812e0cf76717741b6f6674cc4753a3df0c380f29
Arafatmollik1 <arafathossain923@gmail.com>
2020-03-23 11:14:46 +0200

    Updates

---
+++ precache-manifest.95d407739c02c8cebecea33440c38922.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1765bfc32da87ac612dc522d364802",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "201a02a262b9a4666e03",
+    "url": "/weather_app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b2c22ddd37d6c02994c4",
+    "url": "/weather_app/static/css/main.8e787fa6.chunk.css"
+  },
+  {
+    "revision": "201a02a262b9a4666e03",
+    "url": "/weather_app/static/js/2.1c9824d7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather_app/static/js/2.1c9824d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2c22ddd37d6c02994c4",
+    "url": "/weather_app/static/js/main.bc8ce334.chunk.js"
+  },
+  {
+    "revision": "a9980a0bf9982c6775f0",
+    "url": "/weather_app/static/js/runtime-main.719322a1.js"
+  }
+]);
